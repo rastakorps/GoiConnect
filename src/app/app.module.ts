a4f88766024cd68bfc0f4b9949e7234172193f77
Jose Cruz <jose.oakenfold@gmail.com>
2020-09-24 00:16:21 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, IonTabs } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+
+import { SuperTabsModule} from '@ionic-super-tabs/angular';
+import { IonicStorageModule } from '@ionic/storage';
+//import { Camera } from '@capacitor/core';
+import { Camera } from '@ionic-native/camera/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { CartModalPageModule } from './Pages/Social/cart-modal/cart-modal.module';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot(),
+    FormsModule,
+    CartModalPageModule,
+    HttpClientModule,
+    SuperTabsModule.forRoot()
+  ],
   providers: [
     StatusBar,
     SplashScreen,
+    NativeStorage,
+    Camera,
+    File,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
